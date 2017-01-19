3ac1f2252260ae275b40198c786f1382d3b7c968
Flying Katsu <Warped2713@users.noreply.github.com>
2017-01-18 23:12:29 -0500

    finished moving character into a separate module

--- require/character.js
+++ require/character.js
@@ -1,67 +1,182 @@
+const CUSTOM = require("custom.js");
+const ENUM = require("enum.js");
+
 // ===============================================
 // Factory Char properties (to copy or overwrite)
 // ===============================================
 var FactoryChar = function( _input ) {
-  //return function( _input ) {
     let input = _input || {};
-    //this.owner = input.owner || CLIENT.user;
+    this.owner = input.owner || null;
     this.baseKey = input.baseKey || 1;
     this.variantKey = input.variantKey || 0;
-    this.level = 0;
-    this.xp = 0;
-    this.mood = 1;
-    this.numResets = 0;
-    //this.custom = new Customization();
-    // for property in extra, create this.prop
-    //for( let e in extra ) { this[e] = extra[e]; } 
-  //}
+    this.level = input.level || 0;
 };
-
-
 // ===============================================
 // Factory Char methods (for prototypal inheritance)
 // ===============================================
-FactoryChar.prototype.getEmbed = function() {
-  console.log('got embed');
+FactoryChar.prototype.getVariant = function() {
+  return ENUM.Preset.properties[this.baseKey].variants[this.variantKey];
 };
 
 
 // ===============================================
-// Factory Partner properties (to copy or overwrite)
+// SUB CLASSES of FactoryChar
 // ===============================================
+
+// ============ PARTNER =============
 var FactoryPartner = function( _input ) {
   let input = _input || {};
-  this.propA = input.propA || "A";  
+  // Own Properties
+  this.xp = 0;
+  this.mood = 1;
+  this.numResets = 0;
+  this.custom = new CUSTOM.Customization();  
   // Extend FactoryChar
-  let temp = new FactoryChar();
+  let temp = new FactoryChar( input );
   for (t in temp) { 
     if ( temp.hasOwnProperty(t) ) this[t] = temp[t]; 
   }
 };
-
-// ===============================================
-// Factory Partner methods (for prototypal inheritance)
-// ===============================================
-FactoryPartner.prototype.methodB = function() {
-  console.log("method b");
+// Inherited methods for FactoryPartner
+// PARTNER GETTERS
+FactoryPartner.prototype.getEmbed = function( sit, useOC, foot ) {
+  console.log("Partner Embed");
+  return {
+    author: this.getName(),
+    thumb: this.getImg( useOC ),
+    color: this.getColor(),
+    desc: this.getDialogue( sit, ENUM.Feeling.properties[this.mood] ),
+    foot: foot
+  }
 };
+FactoryPartner.prototype.getName = function() {
+    return this.custom.name || this.getVariant().custom.name;
+  };
+FactoryPartner.prototype.getImg = function( useOC ) {
+    if ( useOC && this.custom.img ) {
+      return this.custom.img;
+    } else {
+      return this.getVariant().custom.img;
+    }
+  };
+FactoryPartner.prototype.getColor = function() {
+    return this.custom.color || this.getVariant().custom.color;
+  };
+FactoryPartner.prototype.getCapacity = function() {
+    return this.custom.capacity || this.getVariant().custom.capacity;
+  };
+FactoryPartner.prototype.getAlignment = function() {
+    return this.custom.alignment || this.getVariant().custom.alignment;
+  };
+FactoryPartner.prototype.getPersonality = function() {
+    return this.custom.personality || this.getVariant().custom.personality;
+  };
+FactoryPartner.prototype.getModifier = function( sit ) {
+    return this.custom.modifiers[sit].mod || 
+      ENUM.Personality.properties[ENUM.Personality[this.getPersonality()]].modifiers[sit].mod;
+  };
+FactoryPartner.prototype.getDialogue = function( sit, feeling ) {
+    return this.custom.modifiers[sit].dialogue[feeling] || 
+      ENUM.Personality.properties[ENUM.Personality[this.getPersonality()]].modifiers[sit].dialogue[feeling];
+  };
+// PARTNER SETTERS
+FactoryPartner.prototype.setName = function( v ) {
+    this.custom.name = v;
+  };
+FactoryPartner.prototype.setBase = function( v ) {
+    this.custom.base = v;
+  };
+FactoryPartner.prototype.setImg = function( v ) {
+    this.custom.image = v;
+  };
+FactoryPartner.prototype.setColor = function( v ) {
+    this.custom.color = v;
+  };
+FactoryPartner.prototype.setCapacity = function( v ) {
+    this.custom.capacity = v;
+  };
+FactoryPartner.prototype.setAlignment = function( v ) {
+    this.custom.alignment = v;
+  };
+FactoryPartner.prototype.setPersonality = function( v ) {
+    this.custom.personality = v;
+  };
+FactoryPartner.prototype.setModifier = function( sit, mod ) {
+    this.custom.modifiers[sit].mod = mod;
+  };
+FactoryPartner.prototype.setDialogue = function( sit, feeling, text ) {
+    this.custom.modifiers[sit].dialogue[feeling] = text;
+  };
 // Extend FactoryChar.prototype
 for (f in FactoryChar.prototype) { 
   if ( FactoryChar.prototype.hasOwnProperty(f) ) FactoryPartner.prototype[f] = FactoryChar.prototype[f]; 
 }
+
+// ============= BOSS =============
+var FactoryBoss = function( _input ) {
+  let input = _input || {};
+  // Own Properties
+  this.propA = input.propA || "A";
+  // Defaults
+  input.base = "boss";
+  input.owner = null;  
+  // Extend FactoryChar
+  let temp = new FactoryChar( input );
+  for (t in temp) { 
+    if ( temp.hasOwnProperty(t) ) this[t] = temp[t]; 
+  }
+};
+// Inherited methods for FactoryBoss
+FactoryBoss.prototype.getEmbed = function( text, foot ) {
+  console.log("Boss Embed");
+  return {
+    author: this.getVariant().custom.name,
+    thumb: this.getVariant().custom.img,
+    color: this.getVariant().custom.color,
+    desc: text,
+    foot: foot
+  }
+};
+
+// ============= MINION =============
+var FactoryMinion = function( _input ) {
+  let input = _input || {};
+  // Own Properties
+  this.xp = 0;
+  this.mood = 1;
+  // Defaults
+  input.base = "virus";  
+  // Extend FactoryChar
+  let temp = new FactoryChar( input );
+  for (t in temp) { 
+    if ( temp.hasOwnProperty(t) ) this[t] = temp[t]; 
+  }
+};
+// Inherited methods for FactoryMinion
+FactoryMinion.prototype.getEmbed = function( text, foot ) {
+  console.log("Minion Embed");
+  return {
+    author: this.getVariant().custom.name,
+    thumb: this.getVariant().custom.img,
+    color: this.getVariant().custom.color,
+    desc: text,
+    foot: foot
+  }
+};
+
 
 // ===============================================
 // Factory Char Instantiation (new instances)
 // ===============================================
 var CHARACTER = {
   
-  // Partner Constructor
+  // Constructors
   Partner: FactoryPartner,
-  
-  // Enemy Constructor
-  Enemy: FactoryEnemy
+  Boss: FactoryBoss,
+  Minion: FactoryMinion
   
 };
 
+
 if (Object.freeze) Object.freeze(CHARACTER);
-//module.exports = CHARACTER;
+module.exports = CHARACTER;
